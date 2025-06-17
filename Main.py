0bfb9ddae2c7b625c2f79f7bda6f26911b020411
Franek Domanski <kudlaty.franek@gmail.com>
2025-06-17 17:31:33 +0200

    Updated Prototype

--- Main.py
+++ Main.py
@@ -29,18 +29,52 @@ def get_all_liked_tracks(sp):
 # Fetch all liked songs
 liked_tracks = get_all_liked_tracks(sp)
 
+
+
+def checkIfPlaylistExists(playlist_name_to_find):
+    playlists = []
+    limit = 50
+    offset = 0
+
+    while True:
+        response = sp.current_user_playlists(limit=limit, offset=offset)
+        playlists.extend(response['items'])
+        if response['next']:
+            offset += limit
+        else:
+            break
+
+    for pl in playlists:
+        if pl['name'] == playlist_name_to_find:
+            return True
+        
+    return False
+
+
+if not checkIfPlaylistExists("playlist2000"):
+    playlist2000 = sp.user_playlist_create(user=user_id, name="playlist2000", public=False)
+
+
 # Print first few
-for idx, item in enumerate(liked_tracks[:1], 1):
+for idx, item in enumerate(liked_tracks[:3], 1):
     track = item['track']
-    print(f"{idx}. {track['name']} – {track['artists'][0]['name']}")
     uris = [item['track']['uri']]
+    print (int(track['album']['release_date'].split("-")[0]))
+    if int(track['album']['release_date'].split("-")[0]) >= 2000:
+        sp.playlist_add_items(playlist_id=playlist2000['id'], items=uris)
+        
 
-print(f"\nTotal liked tracks found: {len(liked_tracks)}")
 
-playlist = sp.user_playlist_create(user=user_id, name="CHUJKURWA", public=False)
-print("Nowa playlista ID:", playlist['id'])
 
-sp.playlist_add_items(playlist_id=playlist['id'], items=uris)
-print("Dodano utwory do playlisty.")
+
+
+#print(f"\nTotal liked tracks found: {len(liked_tracks)}")
+
+
+#playlist = sp.user_playlist_create(user=user_id, name="CHUJKURWA", public=False)
+#print("Nowa playlista ID:", playlist['id'])
+#
+#sp.playlist_add_items(playlist_id=playlist['id'], items=uris)
+#print("Dodano utwory do playlisty.")
 
 
