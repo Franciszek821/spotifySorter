5c2e91b1a3682dbc6882b3cd97b50e3333a2f84a
Franek Domanski <kudlaty.franek@gmail.com>
2025-06-18 14:37:26 +0200

    add new app.py and modify Main.py and index.html

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from Main import sort
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+
+load_dotenv()
+
+
+# Spotify auth config
+SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
+SCOPE = "playlist-modify-public playlist-modify-private user-library-read"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    message = None
+    if request.method == "POST":
+        action = request.form.get('action')
+        if action == 'sort':
+            token_info = session.get("token_info", None)
+            if not token_info:
+                return redirect(url_for('login'))
+
+            sp = spotipy.Spotify(auth=token_info['access_token'])
+            user_id = sp.current_user()['id']
+
+            sort(sp, user_id)
+            message = "Sorted playlists!"
+        elif action == 'authorization':
+            return redirect(url_for('login'))
+
+    return render_template("index.html", message=message)
+
+@app.route('/login')
+def login():
+    sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                            client_secret=SPOTIPY_CLIENT_SECRET,
+                            redirect_uri=SPOTIPY_REDIRECT_URI,
+                            scope=SCOPE)
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                            client_secret=SPOTIPY_CLIENT_SECRET,
+                            redirect_uri=SPOTIPY_REDIRECT_URI,
+                            scope=SCOPE)
+
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect(url_for('index'))
+
+def token_required(f):
+    from functools import wraps
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token_info = session.get("token_info", None)
+        if not token_info:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated
+
+if __name__ == "__main__":
+    app.run(debug=True)
