64134ca81df7b7c7f1be4499cf89b4f63d154934
Franek Domanski <kudlaty.franek@gmail.com>
2025-07-05 19:25:42 +0200

    fix: Token refresh

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ import os
 from Main import sort, clear_playlists, top20_songs, artistTop, topArtistsSongs, get_all_playlists, getName
 from dotenv import load_dotenv
 
-
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
-
-
-name = None
-
 
 def get_token():
     sp_oauth = SpotifyOAuth(
@@ -30,15 +25,13 @@ def get_token():
 
     if sp_oauth.is_token_expired(token_info):
         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        session["token_info"] = token_info
 
-    session["token_info"] = token_info
-    return token_info
+    return token_info['access_token']
 
-sp = None
-
-#playlist = ["chuj", "chuj2", "chuj3", "chuj4", "chuj5", "chuj6", "chuj7", "chuj8", "chuj9", "chuj10"]
-
-    
+def get_spotify_client():
+    access_token = get_token()
+    return spotipy.Spotify(auth=access_token, requests_timeout=30)
 
 @app.route("/")
 def main():
@@ -48,28 +41,20 @@ def main():
 @app.route('/about')
 def about_page():
     is_logged_in = "token_info" in session
-
     name = None
     if is_logged_in:
-        token_info = get_token()
-        sp = spotipy.Spotify(auth=token_info['access_token'], requests_timeout=30)
+        sp = get_spotify_client()
         name = getName(sp)
-
     return render_template('About/index.html', is_logged_in=is_logged_in, name=name)
-
 
 @app.route('/help')
 def help_page():
     is_logged_in = "token_info" in session
-
     name = None
     if is_logged_in:
-        token_info = get_token()
-        sp = spotipy.Spotify(auth=token_info['access_token'], requests_timeout=30)
+        sp = get_spotify_client()
         name = getName(sp)
-
     return render_template("Help/index.html", is_logged_in=is_logged_in, name=name)
-
 
 @app.route("/functions", methods=["GET", "POST"])
 def functions():
@@ -77,8 +62,7 @@ def functions():
     if not is_logged_in:
         return redirect(url_for('login'))
 
-    token_info = get_token()
-    sp = spotipy.Spotify(auth=token_info['access_token'], requests_timeout=30)
+    sp = get_spotify_client()
     name = getName(sp)
     playlists = get_all_playlists(sp)
     message = None
@@ -95,20 +79,20 @@ def functions():
             message = "All playlists have been cleared."
         elif action == 'top20_songs':
             selected_time = request.form.get('time')
-            if selected_time != None:
+            if selected_time is not None:
                 top20_songs(sp, selected_time)
                 message = "Top 20 songs playlist has been created."
             else:
                 message = "Choose time"
         elif action == 'top10_artist':
             selected_artist = request.form.get('artist')
-            if selected_artist != None:
+            if selected_artist is not None:
                 message = artistTop(sp, selected_artist)
             else:
                 message = "Choose artist"
         elif action == 'topArtistsSongs':
             selected_timeArt = request.form.get('timeTopArtist')
-            if selected_timeArt != None:
+            if selected_timeArt is not None:
                 topArtistsSongs(sp, selected_timeArt)
                 message = "Playlist have been created"
             else:
@@ -117,7 +101,6 @@ def functions():
         playlists = get_all_playlists(sp)
 
     return render_template("Functions/index.html", message=message, my_list=playlists, is_logged_in=is_logged_in, name=name)
-
 
 @app.route('/login')
 def login():
@@ -133,10 +116,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()  # Clear all session data (token, etc.)
-    return redirect(url_for('main'))  # Redirect to home or login page
-
-
-
+    return redirect(url_for('main'))
 
 @app.route('/callback')
 def callback():
@@ -148,13 +128,9 @@ def callback():
     )
 
     code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code, as_dict=True)
+    token_info = sp_oauth.get_access_token(code)
     session["token_info"] = token_info
     return redirect(url_for('main'))
-
-
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
